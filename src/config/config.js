98031541602aa1131a255b58b6700b9654a5f1c0
Facundo Rodríguez <facundo.rodriguez@deephow.com>
2022-06-21 16:04:16 -0300

    Added OAuth login with facebook

--- src/config/config.js
+++ src/config/config.js
@@ -13,6 +13,15 @@ const config = {
     storageBucket: process.env.FIREBASESTORAGEBUCKET,
     google_creds: process.env.GOOGLE_CREDS ? JSON.parse(process.env.GOOGLE_CREDS) : {},
   },
+  FACEBOOK: {
+    oauthUrl: 'https://www.facebook.com/v14.0/dialog/oauth',
+    tokenUrl: 'https://graph.facebook.com/v14.0/oauth/access_token',
+    graphAPI: 'https://graph.facebook.com/',
+    client_id: process.env.FACEBOOKID,
+    secret: process.env.FACEBOOKSECRET,
+    redirect_uri: process.env.FACEBOOKREDIRECT,
+    scopes: ['email', 'public_profile'],
+  },
 };
 
 const logger = winston.createLogger(config.LOG_CONFIGURATION);
--- src/services/formatQuery/formatQuery.service.js
+++ src/services/formatQuery/formatQuery.service.js
@@ -1,20 +1,13 @@
-import { logger } from '../../config/config.js';
-
 export default class FormatQueryService {
   /**
    * Format query for @param rawData
    * @param {import('firebase-admin').firestore.QueryDocumentSnapshot[]} rawData
    */
   static formatQuery(rawData) {
-    try {
-      const prunnedData = [];
-      rawData.forEach((value) => {
-        prunnedData.push(value.data());
-      });
-      return prunnedData;
-    } catch (error) {
-      logger.error(`Error formatting query data ${JSON.stringify(error)}`);
-      throw error;
-    }
+    const prunnedData = [];
+    rawData.forEach((value) => {
+      prunnedData.push(value.data());
+    });
+    return prunnedData;
   }
 }
